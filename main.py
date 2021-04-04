5f381cf6a86453441813af588c6ab2e90d90f9eb
carloscontrerasruiz <carloscr9018@gmail.com>
2021-04-04 01:48:47 -0600

    Version funcional imagenes por carpeta

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import logging
 import os
 import requests
 from bs4 import BeautifulSoup # to parse HTML
+
+logging.basicConfig(level=logging.INFO,format='%(asctime)s:%(message)s')
 
 BASE_URL = 'https://agot.theangels.eu'
 SAVE_FOLDER = 'imagenes'
@@ -9,69 +12,70 @@ def main():
     if not os.path.exists(SAVE_FOLDER):
         os.mkdir(SAVE_FOLDER)
     n_categorias = int(input('Cuantas categorias? '))
-    n_imagenes = int(input('Cuantas imagenes descargaras? '))
-    createBinders(dowloand_categories(), n_categorias)
-    imagesUrlList = getImagesUrl(getImagesPageUrl(n_imagenes))
-    print('Lista de url de la imagen obtenidas')
-    print(imagesUrlList)
+    n_imagenes = int(input('Cuantas imagenes descargaras por categoria? '))
+    imagesUrlList = getImagesUrl(getImagesPageUrl(n_categorias,n_imagenes))
+    logging.info('Lista de url de la imagen obtenidas')
     download_images(imagesUrlList)
 
-def dowloand_categories():
-    print('Iniciando creacion de carpetas para imagenes')
-    with open("page.html") as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
-        results = soup.findAll('span', {'class': 'resultSpacer_title'})
-        category_list = []
-        for re in results:
-            category_list.append(re.text)
-    print(f'Numero de categorias encontradas {len(category_list)}')
-    return category_list
-
-def createBinders(categories_list, n_categorias):
-    numeroCarpetas = 0
-    for category in categories_list:
-        category = category.strip()
-        category = category.replace(" ", "")
-        if numeroCarpetas < n_categorias:
-            if not os.path.exists(SAVE_FOLDER +'/'+ category):
-                os.mkdir(SAVE_FOLDER +'/'+ category)
-        else:
-            break
-        numeroCarpetas += 1
-    print(f'Se crearon {numeroCarpetas} carpetas')
+def createBinder(category):
+    category = category.strip().replace(" ", "").replace("'", "")
+    if not os.path.exists(SAVE_FOLDER +'/'+ category):
+        os.mkdir(SAVE_FOLDER +'/'+ category)
+    logging.info('Se creo la carpeta '+category)
+    return category
         
-def getImagesPageUrl(n_imagenes):
-    print('Iniciando obtencion de url de la pagina donde esta la imagen')
+
+def getImagesPageUrl(n_categorias = 5,n_imagenes=5):
+    logging.info('Se inicia busqueda las categorias y la pagina del detalle de las imagenes')
     with open("page.html") as fp:
         soup = BeautifulSoup(fp, 'html.parser')
-        results = soup.findAll('div', {'class': 'cardLine'}, limit=n_imagenes)
+        results = soup.findAll('div', {'class': 'resultSpacer'}, limit=n_categorias)
         img_url_list = []
-        for re in results:
-            img_url_list.append(re.a['href'])
+        for tag in results:
+            contadorImagenes = 0
+            categoryName = createBinder(tag.span.text)
+            while True:
+                tag = tag.nextSibling
+                if(tag['class'][0] != 'resultSpacer' and contadorImagenes < n_imagenes):
+                    img_url_list.append({
+                        "imagePageUrl":tag.a['href'],
+                        "category":categoryName
+                    })
+                    contadorImagenes += 1
+                else:
+                    break
+    logging.info('Se obtienen %s paginas del detalle de la imagen',len(img_url_list))
     return img_url_list
 
+def cleanRoute(imageName):
+    return imageName.strip().replace(" ", "").replace("'", "")
+
 def getImagesUrl(imgpage_url_list):
-    print('Se ingresa a la pagina de la imagen para obtener su url')
+    logging.info('Se ingresa a la pagina del detalle de cada imagen para obtener su url')
     img_url = []
     for search_url in imgpage_url_list:
-        response = requests.get(search_url)
+        logging.info('Peticion a la pagina %s',search_url["imagePageUrl"])
+        response = requests.get(search_url["imagePageUrl"])
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
-        results = soup.findAll('img',{'id':'cardImg1'})
-        for re in results:
-            img_url.append(BASE_URL + re['src'])
+        results = soup.find('div',{'class':'boxRight'})
+        imageName = soup.find('div',{'id':'card_name'})
+        img_url.append({
+            "imageUrl":BASE_URL + results.img['src'],
+            "category":search_url["category"],
+            "imageName":cleanRoute(imageName.a.text)
+        })
     return img_url
 
 def download_images(img_url_list):
-    print('Start dowloand images...')
-    contador = 1
+    logging.info('Inicia descarga de imagenes...')
     for url in img_url_list:
-         response = requests.get(url)
-         imagename = SAVE_FOLDER + '/core' + str(contador) + '.jpg'
-         with open(imagename, 'wb') as file:
-             file.write(response.content)
-         contador += 1
-    print('Finaliza la descraga')
+        logging.info('Descargando imagen %s',url['imageName'])
+        response = requests.get(url['imageUrl'])
+        imagename = SAVE_FOLDER + '/' + url['category'] + '/' + url['imageName'] + '.jpg'
+        with open(imagename, 'wb') as file:
+            file.write(response.content)
+    logging.info('Finaliza la descraga')
 
 
 if __name__ == '__main__':
